010fd52e851a8f9b507ad8ea982239026ff1a537
AyushRawat4 <136440500+AyushRawat4@users.noreply.github.com>
2024-03-12 16:34:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be highL
+
+console.log("Ayush Singh Rawat")
+
+let name = "ayush"
+let age = 22
+let isLoggedIn = true
+
+// numbers => 2 to power 53 or something
+// bigint  => numbers range more than the above numbers
+// string  => ""
+// boolean  => true/false
+// null   => standalone value 
+//  undefined  => value is not defined nor zero or null
+
+//  symbol => unique
+// object => 
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);   // object 
+console.log(typeof undefined);    // undefined
