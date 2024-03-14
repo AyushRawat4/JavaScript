db1ab4779f05dd0ed6ead173916dd10479596f13
AyushRawat4 <136440500+AyushRawat4@users.noreply.github.com>
2024-03-14 13:46:22 +0000

    conversion and comparisons

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
   let score = undefined
 
-  console.log(typeof score);
-  console.log(typeof (score));
+  // console.log(typeof score);
+  // console.log(typeof (score));
 
   let inNumber = Number(score)
-  console.log(typeof inNumber);
-  console.log(inNumber);
+  // console.log(typeof inNumber);
+  // console.log(inNumber);
 
     // 33        => 33
    // "33abc"   => NaN (not a number) [datatype number]
@@ -16,11 +16,11 @@
 
 let isLoggedIn = 0
 let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn)
+// console.log(booleanLoggedIn)
 
 let isLoggedIn1 = "ayush"
 let booleanLoggedIn1 = Boolean(isLoggedIn1)
-console.log(booleanLoggedIn1)
+// console.log(booleanLoggedIn1)
 
 // 1 => true ; 0 => false
 // "" => false
@@ -29,5 +29,33 @@ console.log(booleanLoggedIn1)
 let someNumber = 32
 let stringSomeNumber = String(someNumber)
 
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+// *******************************Operations****************************************
+
+
+let value = 33
+let newValue = -value
+// console.log(newValue);
+
+// console.log(2+2); 
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "kaisa "
+let str2 = "hai"
+let str3 = str1+str2
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "4");
+
+
+console.log(+true);
+console.log(+"");
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// datatypes of comparing operators must be same or else it would give wrong outputs
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);  // in comparison and equality check they convert null in different values . as in comparison it convert null to 0.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+//  ===
+
+console.log("2" === 2);
