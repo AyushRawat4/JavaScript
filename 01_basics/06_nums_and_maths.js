b19ea7f728b4233f3effb83181bfa29d993e68a9
AyushRawat4 <136440500+AyushRawat4@users.noreply.github.com>
2024-03-19 13:13:00 +0000

    num and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const anyNumber = 100
+// console.log(anyNumber);
+
+
+const number = new Number(200)
+// console.log(number);
+
+// console.log(number.toString().length);
+
+// console.log(number.toFixed(2));
+
+const newNumber = 30.9876
+// console.log(newNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// *************************************** M A T H S ******************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-45));
+// console.log(Math.round(45.6));
+// console.log(Math.ceil(89.3));
+// console.log(Math.floor(45.9));
+
+// console.log(Math.min(4 , 5, 4, 6 ,7 ));
+// console.log(Math.max(4 , 5, 4, 6 ,7 ));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+      console.log(Math.floor((Math.random()* (max - min +1))) + min);
+
+
