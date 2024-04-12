4774d368aef84348e9c5f0c9e60798f7c2dc90b4
AyushRawat4 <136440500+AyushRawat4@users.noreply.github.com>
2024-04-12 18:33:32 +0000

    ifelse_OrAndOperators

---
+++ 04_control_flow/one.js
@@ -0,0 +1,63 @@
+//   if   
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+
+}
+
+// const temprature = 90
+
+// if(temprature < 50) {
+//      console.log(`temprature is less then 50 degree`);
+// }
+// else {
+// console.log(`temprature is greater than 50 degree `);
+// }
+
+// console.log(`1Executed`);
+
+// const num = 200
+// if (num>100) {
+//     let power = 'flyy'
+//     console.log(`power of user : ${power}`);
+// }
+// console.log(`power of user : ${power}`);                                    will give error bcoz of block scope
+
+// < , > , <= , >= , == , != , === , !==  
+
+
+
+const balance = 1000
+// if(balance > 500 ) console.log('testee'),console.log('test222');   // can also write this but not a good practice always use {}
+
+// if (balance < 500) {
+//     console.log(`1less than 500`);
+// }
+// else if (balance < 750) {
+//     console.log(`1less than 750`);
+// }
+// else if (balance < 900) {
+//     console.log(`1less than 900`);
+// }
+// else if (balance < 1000) {
+//     console.log(`1less than 1000`);
+// }else console.log(`greater than 1000`);
+
+const userLoggedIN = true
+const debitCard = true
+
+if (userLoggedIN && debitCard && 2==3) {
+    
+    console.log(`allow to buy course `);
+}
+
+const isUserLoggedInByEmail = false 
+const isUserLoggedInByGmail = true
+
+if (isUserLoggedInByGmail || isUserLoggedInByEmail) {
+
+    console.log(`let user logg in `);
+    
+}
+
