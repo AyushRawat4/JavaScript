bd064029d81ab21550f806214b372587ff28c3f7
AyushRawat4 <136440500+AyushRawat4@users.noreply.github.com>
2024-04-18 14:29:49 +0000

    while do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//  WHILE LOOP 
+
+let index = 0
+while (index <= 10) {
+
+    console.log(`index value is ${index}`);
+    index = index+2
+    
+}
+
+let myArray = ['Megaknight',"firecracker","witch",'wizard ','valkrye',"ice golem"]
+let i=0
+while (i<myArray.length) {
+    console.log(`value of ${i} is : ${myArray[i]}`);
+    i++
+}
+
+
+//  DO WHILE LOOP 
+let j=12
+do {
+    console.log(`hello world ${j}`)
+    j++
+} while (j<=10);
